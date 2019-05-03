15becc91991afabdcc3d0b79e2a59f32c1ca35c7
jarandaba <jarandaba@CC403.itam.mx>
2019-05-03 14:55:53 -0500

    esta hecho el signup y creartabla

--- OmegaElbueno/src/java/webservices/SoapWS.java
+++ OmegaElbueno/src/java/webservices/SoapWS.java
@@ -16,6 +16,8 @@ import javax.jws.WebService;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
 import javax.ejb.Stateless;
+import javax.json.Json;
+import org.json.simple.JSONObject;
 
 /**
  *
@@ -63,17 +65,18 @@ public class SoapWS {
     /**
      * Web service operation
      */
-    @WebMethod(operationName = "signIn")
-    public Boolean signIn(@WebParam(name = "base") String base, @WebParam(name = "usuario") String usuario, @WebParam(name = "password") String password) {
+    @WebMethod(operationName = "signUp")
+    public Boolean signUp(@WebParam(name = "base") String base, @WebParam(name = "usuario") String usuario, @WebParam(name = "password") String password) {
         try {
             Class.forName("org.apache.derby.jdbc.ClientDriver");
             Connection con = DriverManager.getConnection("jdbc:derby://localhost:1527/root","root","root");
             Statement query = con.createStatement();
             ResultSet rs = query.executeQuery("SELECT MAX(ID) AS RES FROM USUARIOS");
-            int id=Integer.parseInt(rs.getString("RES"));
+            rs.next();
+            int id=rs.getInt("RES");
             System.out.println(id);
             id++;
-            rs = query.executeQuery("INSERT INTO USUARIOS (NOMBRE,BASE, PASSWORD,ID) VALUES ('"+usuario+"','"+base+"','"+password+"',"+id+")");
+            query.execute("INSERT INTO USUARIOS (NOMBRE,BASE, PASSWORD,ID) VALUES ('"+usuario+"','"+base+"','"+password+"',"+id+")");
             System.out.println(rs.toString());
             con.commit();
             con.close();
@@ -87,6 +90,40 @@ public class SoapWS {
         }
         return false;
       
+    }
+
+    /**
+     * Web service operation
+     */
+    @WebMethod(operationName = "crearTabla")
+    public Boolean crearTabla(@WebParam(name = "esquema") String esquema, @WebParam(name = "nombreTabla") String nombreTabla, @WebParam(name = "iDUsuario") int iDUsuario) {
+            try {
+            Class.forName("org.apache.derby.jdbc.ClientDriver");
+            Connection con = DriverManager.getConnection("jdbc:derby://localhost:1527/root","root","root");
+            Statement query = con.createStatement();
+            ResultSet rs = query.executeQuery("SELECT MAX(IDTABLA) AS RES FROM TABLAS");
+            rs.next();
+            int iDTabla=rs.getInt("RES");
+            iDTabla++;
+            String s="INSERT INTO ROOT.TABLAS (NOMBRE,IDTABLA,IDUSUARIO) VALUES ('"+nombreTabla+"',"+iDTabla+","+iDUsuario+")";
+            query.execute(s);
+            
+            //Aquí hay que desempaquetar el JSON y generar el query SQL que hará la nueva tabla.
+//          String parametros="";
+//          esquema.
+//            
+//          query.executeQuery("CREATE TABLE "+iDTabla+"( )");
+            con.commit();
+            con.close();
+            
+            
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(SoapWS.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(SoapWS.class.getName()).log(Level.SEVERE, null, ex);
+            
+        }
+        return false;
     }
     
     
