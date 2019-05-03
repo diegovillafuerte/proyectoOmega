4107ca0305504b3e66f9f331efed3a2634f2623e
diegovillafuerte <diegovillafuertesoraiz@gmail.com>
2019-05-03 13:46:49 -0500

    sql queries

---
+++ OmegaElbueno/build/web/WEB-INF/classes/crearDB.sql
@@ -0,0 +1,29 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Author:  diego
+ * Created: May 3, 2019
+ */
+
+CREATE TABLE USUARIOS
+(
+id int
+nombre varchar(100),
+password data_type(100),
+base data_type(100),
+PRIMARY KEY (id)
+);
+
+CREATE TABLE TABLAS
+(
+idtabla int
+idusuario int,
+nombre varchar(100),
+PRIMARY KEY (idtabla)
+);
+
+INSERT INTO ROOT.USUARIOS (ID, NOMBRE, PASSWORD, BASE) 
+	VALUES (1, 'MIGUELON', 'MIGUELON', 'MIGUELON')
